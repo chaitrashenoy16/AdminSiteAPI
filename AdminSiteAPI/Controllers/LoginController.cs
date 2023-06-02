0e8a297bd2d7f735739443a7fe7fdf2e28b66054
ESOLUTIONS\cshenoy <chaitra.shenoy@Trivium-esolutions.com>
2023-06-02 09:35:54 +0530

    login controller and login added

---
+++ AdminSiteAPI/Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using AdminSiteAPI.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace AdminSiteAPI.Controllers
+{
+    [ApiController]
+    public class LoginController : Controller
+    {
+        [HttpPost]
+        [Route("api/login")]
+        public bool doLogin([FromBody] Login login)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=CHAITRASHEN-LT;Initial Catalog=AdminSite;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from LoginUsers where username=@username AND password=@password", con);
+            cmd.Parameters.AddWithValue("@username", login.username);
+            cmd.Parameters.AddWithValue("@password", login.password);
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ AdminSiteAPI/Model/Login.cs
@@ -0,0 +1,8 @@
+﻿namespace AdminSiteAPI.Model
+{
+    public class Login
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
